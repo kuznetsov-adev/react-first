846c87eb53dc3d61ea82defcd9e8a04c73410fa0
g.smith <kuznetsov.adev@gmail.com>
2022-04-12 21:22:37 +0500

    React begin: init project & useState()

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+/**
+ * <App /> - компонент который нужно отрисовать
+ * 'root' - блок в который мы монтируем компонент
+ * */
+ReactDOM.render(
+    <App/>,
+  document.getElementById('root')
+);
