7a3740480f3497b12e40edad5242788245c113a8
g.smith <kuznetsov.adev@gmail.com>
2022-04-13 17:40:40 +0500

    React begin: add 2 way binding

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 function App() {
     //useState - f() изменения состояния в DOM дереве
     const [likes, setLikes] = useState(0)
+    const [value, setValue] = useState('Text inside input')
 
     function increment() {
         setLikes(likes + 1)
@@ -16,6 +17,13 @@ function App() {
     return (
         <div className="App">
             <h1>{likes}</h1>
+            <h1>{value}</h1>
+            <input
+                type="text"
+                value={value}
+                //двустороннее связывание
+                onChange={ event => setValue(event.target.value) } //event.target - DOM element; value - значение
+            />
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
         </div>
