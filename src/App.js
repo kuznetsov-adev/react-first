f8cb9ff794cd9e21785466bf64316497c933432f
g.smith <kuznetsov.adev@gmail.com>
2022-04-20 21:20:27 +0500

    React begin: add decomposition for PostFilter

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, {useMemo, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
-import CustomSelect from "./components/UI/select/CustomSelect";
-import CustomInput from "./components/UI/input/CustomInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
     const [posts, setPosts] = useState([
-        {id: 1, title: 'A_Javascript', description: 'F_Javascript - programming language'},
-        {id: 2, title: 'B_Javascript 2', description: 'E_Javascript - programming language'},
-        {id: 3, title: 'C_Javascript 3', description: 'D_Javascript - programming language'}
+        {id: 1, title: 'Kotlin', description: 'backend programming language'},
+        {id: 2, title: 'Javascript', description: 'frontend - programming language'},
+        {id: 3, title: 'Java', description: 'backend - programming language'}
     ])
-    const [selectedSort, setSelectedSort] = useState('')
-    const [searchQuery, setSearchQuery] = useState('')
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    // const [selectedSort, setSelectedSort] = useState('')
+    // const [searchQuery, setSearchQuery] = useState('')
 
     const sortedPosts = useMemo(() => {
-        if (selectedSort) {
-            return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort.toLowerCase()].localeCompare(b[filter.sort.toLowerCase()]))
         }
         return posts
-    }, [selectedSort, posts])
+    }, [filter.sort, posts])
 
     const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(searchQuery))
-    }, [searchQuery, sortedPosts])
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
+    }, [filter.query, sortedPosts])
 
     function createPost(newPost) {
         setPosts([...posts, newPost])
@@ -33,30 +33,11 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id))
     }
 
-    function sortPosts(sort) {
-        setSelectedSort(sort)
-    }
-
     return (
         <div className="App">
-            <CustomInput
-                value={searchQuery}
-                onChange={e => setSearchQuery(e.target.value)}
-                placeholder="Search..."
-            />
             <PostForm create={createPost}/>
             <hr style={{margin: '15px 0'}}/>
-            <div>
-                <CustomSelect
-                    value={selectedSort}
-                    onChange={sortPosts}
-                    defaultValue="Sorting by"
-                    options={[
-                        {value: 'title', name: 'By name'},
-                        {value: 'description', name: 'By description'}
-                    ]}
-                />
-            </div>
+            <PostFilter filter={filter} setFilter={setFilter}/>
 
             {sortedAndSearchedPosts.length
                 ?
